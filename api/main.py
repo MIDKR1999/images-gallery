568c354c0d691e01cad0a844e4ccaf2b060f4318
midhun <midhunkrishnan.t@gmail.com>
2023-06-17 16:42:04 +0530

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,11 @@
-print("Hello from python")
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":      #if main.py is called directly using python3 main.py, then name is set as __main__ and the app get exectued 
+    app.run(host="0.0.0.0", port = 5050)
